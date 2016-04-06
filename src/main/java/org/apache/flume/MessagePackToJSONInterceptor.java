58488d111016dbd7d6f01753c8b118c068964013
ashuiskov <ashuiskov@ebay.com>
2016-04-06 14:14:11 +0200

    Added optional GZIP stream support + auto-detect of stream type + system-local new line separators

--- src/main/java/org/apache/flume/MessagePackToJSONInterceptor.java
+++ src/main/java/org/apache/flume/MessagePackToJSONInterceptor.java
@@ -11,9 +11,12 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 public class MessagePackToJSONInterceptor implements Interceptor {
     private final Logger log = Logger.getLogger(MessagePackToJSONInterceptor.class);
+
+    private static final byte[] NEW_LINE_BYTES = System.getProperty("line.separator").getBytes();
 
     private static final TypeReference<Object> typeReference = new TypeReference<Object>() {};
 
@@ -25,15 +28,28 @@ public class MessagePackToJSONInterceptor implements Interceptor {
         jsonMapper = new ObjectMapper();
     }
 
+    public static InputStream getInputStream(InputStream input) throws IOException {
+        PushbackInputStream pb = new PushbackInputStream(input, 2);
+        byte[] signature = new byte[2];
+        pb.read(signature);
+        pb.unread(signature);
+        if (signature[0] == (byte) 0x1f && signature[1] == (byte) 0x8b) {
+            return new GZIPInputStream(pb);
+        } else {
+            return pb;
+        }
+    }
+
     public Event intercept(Event event) {
-        InputStream is = new ByteArrayInputStream(event.getBody());
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         try {
+            InputStream is = getInputStream(new ByteArrayInputStream(event.getBody()));
             JsonParser parser = msgpMapper.getFactory().createParser(is);
             Iterator<Object> objectIterator = parser.readValuesAs(typeReference);
             while (objectIterator.hasNext()) {
                 Object next = objectIterator.next();
                 jsonMapper.writeValue(os, next);
+                os.write(NEW_LINE_BYTES);
             }
             event.setBody(os.toByteArray());
         } catch (IOException e) {
